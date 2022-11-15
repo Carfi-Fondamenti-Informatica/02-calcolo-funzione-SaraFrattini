329146d6f9d92d6f45ae112974b11471d48fbca5
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-11-15 09:24:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    float a,b,x,y;
+    float z;
+    cin >> a;
+    cin >> b;
+    cin >> x;
+    cin >> y;
+    if (x<0 && y>0) {
+        z=(a*x)-(b*y);
+        cout << z << endl;
+    }
+    else if (x>=0 && y<=0) {
+        z=(a*(x*x))-(b*y);
+        cout << z << endl;
+    }
+    else {
+        z=(a*x)+(b*(y*y));
+        cout << z << endl;
+
+    }
+    return 0;
 }
